6e677cf0e57eb2e94ac69d4b934495d383e2477d
No-Hyunji <kiekuy47@gmail.com>
2021-01-04 00:59:01 +0900

    태그상수에저장

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberInput = document.querySelector('#input');
+const clearButton = document.querySelector('#clear');
+const plusButton = document.querySelector('#plus');
+const minusButton = document.querySelector('#minus');
+const divideButton = document.querySelector('#divide');
+const multiplyButton = document.querySelector('#multiply');
+const resultButton = document.querySelector('#result');
